7f9be0db56a89d146fab58577fbd2392a62dd1fd
alekoChe <aleko2117bis@gmail.com>
2022-06-13 18:49:48 +0300

    delete CartService.java

--- src/main/java/ru/gbAleko/springLesson10_HW_3/models/Cart.java
+++ src/main/java/ru/gbAleko/springLesson10_HW_3/models/Cart.java
@@ -10,6 +10,7 @@ import ru.gbAleko.springLesson10_HW_3.models.entities.Buyer;
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Data
 @NoArgsConstructor
@@ -17,12 +18,34 @@ import java.util.Map;
 @Component
 public class Cart {
 
-    private Buyer buyer;
+    //private Buyer buyer;
+    private ProductDto productDto;
+    private Integer numberProducts;
 
     private Map<ProductDto, Integer> cartList;
 
     @PostConstruct
     public void init() {
         cartList = new HashMap<>();
+    }
+
+    //public void deleteProductFromCartList(Long id) {
+      //  cartLList.removeIf(x -> x.getId().equals(id));
+    //}
+    private void deleteProductFromCartList(Long id) {
+        //ProductDto pr = p -> p.
+        Optional<ProductDto> productDto = cartList.entrySet().stream()
+                .filter(getProductDto()).findFirst();
+    }
+    private void addProductIntoCart(ProductDto productDto, Integer numberProducts) {
+        if (!cartList.containsKey(productDto)) {
+            cartList.put(productDto, numberProducts);
+        } else {
+            cartList.put(productDto, cartList.get(productDto) + numberProducts);
+        }
+    }
+
+    private Map<ProductDto, Integer> getCartList() {
+        return cartList;
     }
 }
--- src/main/java/ru/gbAleko/springLesson10_HW_3/services/CartService.java
+++
@@ -1,4 +0,0 @@
-package ru.gbAleko.springLesson10_HW_3.services;
-
-public class CartService {
-}
